ab757d87cc92981ce51888b6587ddf3ef6b55ddf
NhatTran2608 <112406282+NhatTran2608@users.noreply.github.com>
2023-02-16 12:22:46 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const cors = require("cors")
+const contactsRouter = require("./app/router/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
-const contactsRouter = require("./app/routes/contact.route");
-app.use("/api/contacts", contactsRouter);
 app.use(cors());
 app.use(express.json());
 
@@ -11,5 +11,20 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: "Welcom to contact book application." });
 });
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
+
 
 module.exports = app;
